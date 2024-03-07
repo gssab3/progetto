54439d8280f2f7584d1c28eb32e1677a7b2e3afa
gssab3 <g.sabetta3@studenti.unisa.it>
2024-03-07 15:31:22 +0100

    modified message in the main

--- src/it/unisa/prova/Main.java
+++ src/it/unisa/prova/Main.java
@@ -2,8 +2,9 @@ package it.unisa.prova;
 //Titolo d'esempio per commit
 public class Main {
 
+	//Main
 	public static void main(String[] args) {
-		System.out.println("tubetto");
+		System.out.println("tubetto main");
 	}
 
 }
